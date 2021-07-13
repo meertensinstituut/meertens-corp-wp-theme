a8c27031147aaa1a494f6aef25b519eec3a0c896
BD <basdoppen@gmail.com>
2021-07-13 09:13:27 +0200

    Update content-card01.php
    excerpt

--- meertens-corp-wp-theme/template-parts/content-card01.php
+++ meertens-corp-wp-theme/template-parts/content-card01.php
@@ -1,5 +1,5 @@
 <div class="mCardHome">
 	<div>2 juni 2021</div>
 	<div><?php the_title(); ?></div>
-	<div><?php the_content(); ?></div>
+	<div><?php echo get_the_excerpt() ?><br><a href="<?php the_permalink(); ?>">Read more</a></div>
 </div>
