6a1321f3f9fa44fe240dc5ad35265c1257a21eee
BD <basdoppen@gmail.com>
2021-07-13 09:27:17 +0200

    Update content-card01.php

--- meertens-corp-wp-theme/template-parts/content-card01.php
+++ meertens-corp-wp-theme/template-parts/content-card01.php
@@ -1,5 +1,5 @@
 <div class="mCardHome">
 	<div style="font-size:.8rem; color:#666;"><?php echo get_the_date('d-m-Y'); ?></div>
-	<div><?php the_title(); ?></div>
+	<div><strong><?php the_title(); ?></strong></div>
 	<div><?php echo get_the_excerpt() ?><br><a href="<?php the_permalink(); ?>">Read more</a></div>
 </div>
