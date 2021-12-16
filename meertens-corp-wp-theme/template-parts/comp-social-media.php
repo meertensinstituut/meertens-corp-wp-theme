e64a4e9429aa811820ac1df6e3c1e48ae5260511
Bas Doppen <basdoppen@gmail.com>
2021-12-16 11:09:55 +0100

    Update comp-social-media.php

--- meertens-corp-wp-theme/template-parts/comp-social-media.php
+++ meertens-corp-wp-theme/template-parts/comp-social-media.php
@@ -2,4 +2,7 @@
 	<a href="https://twitter.com/meertens_knaw" target="_blank"><span class="mIcon"><img src="<?php bloginfo('template_url'); ?>/images/icon-tw.png" alt="Volg ons op Twitter"></span>Volg ons op Twitter</a><br>
 	<a href="https://www.facebook.com/MeertensInstituut" target="_blank"><span class="mIcon"><img src="<?php bloginfo('template_url'); ?>/images/icon-fb.png" alt="Volg ons op Facebook"></span>Volg ons op Facebook</a><br>
 	<a href="<?php bloginfo('rss2_url'); ?>" target="_blank"><span class="mIcon"><img src="<?php bloginfo('template_url'); ?>/images/icon-rss.png" alt="Volg ons via RSS"></span>Volg ons via RSS </a>
+
+	<br><br>
+	<a href="index.php/2010/08/13/nieuwsbrief/">Meld je aan voor onze nieuwsbrief.</a>
 </div>
