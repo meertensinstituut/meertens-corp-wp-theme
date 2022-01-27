0e91a651dd7597145d035429d94460a5ac6a108e
Bas Doppen <basdoppen@gmail.com>
2022-01-27 16:17:33 +0100

    Update header.php

--- meertens-corp-wp-theme/header.php
+++ meertens-corp-wp-theme/header.php
@@ -39,6 +39,17 @@ if ( is_user_logged_in() ) {
 </head>
 <body class="<?php echo $bodyClass ?>">
 
+  <!-- Google Tag Manager -->
+  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KXNM34"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KXNM34');</script>
+  <!-- End Google Tag Manager -->
+
+
 	<div class="screenReaderMenu">
     <a href="#main">skip to main content</a>
   </div>
